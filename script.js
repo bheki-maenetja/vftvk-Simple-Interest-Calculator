57de2bd4b51f3fa174bc2c43edcea2cf0cb8e46d
bheki-maenetja <bhekimaenetja@gmail.com>
2021-08-31 12:34:06 +0200

    completed js tasks

--- script.js
+++ script.js
@@ -1,6 +1,17 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  p = document.getElementById('principal').value
+  var principal = document.getElementById('principal').value
+  var rate = document.getElementById('rate').value
+  var years = document.getElementById('years').value
+  var interest = principal * years * rate / 100
+  var year = new Date().getFullYear() + parseInt(years)
+
+  var result = document.querySelector('#result')
+  result.innerText = `If you deposit ${principal}\nat an interest rate of ${rate}%\nyou will receive an amount of ${interest}, in the year ${year}`
+}
+
+function updateRate() {
+  var rateval = document.getElementById('rate').value
+  document.getElementById('rate_val').innerText = rateval
 }
         
